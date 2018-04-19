47f35326e1b18a365721939a911b02b49ffa6c7d
German Schmidt <gerschmidtt@gmail.com>
2018-04-19 00:18:44 -0300

    Se suben estilos generales y paquetes angular nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
-import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
-import { LoginComponent } from './componentes/login/login.component';
-//  import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
+import { MatIconModule, MatFormFieldModule, MatInputModule  } from '@angular/material';
 
+import { AgmCoreModule } from '@agm/core';
+
+//  import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 // import { AccordionModule } from 'ngx-bootstrap';
 // agrego las clases para utilizar ruteo
 import { RouterModule, Routes } from '@angular/router';
 
-import { MiHttpService } from './servicios/mi-http/mi-http.service'; 
-import { PaisesService } from './servicios/paises.service'; 
 
-import { JugadoresService } from './servicios/jugadores.service'; 
-import{ ArchivosJugadoresService} from './servicios/archivos-jugadores.service'; 
+// MIS COMPONENTES
+import { AppComponent } from './app.component';
+import { AdivinaElNumeroComponent } from './componentes/adivina-el-numero/adivina-el-numero.component';
+import { ListadoDeResultadosComponent } from './componentes/listado-de-resultados/listado-de-resultados.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ArchivosJugadoresService } from './servicios/archivos-jugadores.service';
 import { ErrorComponent } from './componentes/error/error.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { AgilidadAritmeticaComponent } from './componentes/agilidad-aritmetica/agilidad-aritmetica.component';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { AdivinaMasListadoComponent } from './componentes/adivina-mas-listado/adivina-mas-listado.component';
 import { AgilidadMasListadoComponent } from './componentes/agilidad-mas-listado/agilidad-mas-listado.component';
-import { RuteandoModule } from './ruteando/ruteando.module';
 import { ListadoComponent } from './componentes/listado/listado.component';
+import { JugadoresListadoComponent } from './componentes/jugadores-listado/jugadores-listado.component';
+import { ListadosComponent } from './componentes/listados/listados.component';
+import { JuegosComponent } from './componentes/juegos/juegos.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { MenuCardComponent } from './componentes/menu-card/menu-card.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
+import { ListadoDePaisesComponent } from './componentes/listado-de-paises/listado-de-paises.component';
+import { MapaDeGoogleComponent } from './componentes/mapa-de-google/mapa-de-google.component';
+import { InputJugadoresComponent } from './componentes/input-jugadores/input-jugadores.component';
+
+import { RuteandoModule } from './ruteando/ruteando.module';
+
+import { MiHttpService } from './servicios/mi-http/mi-http.service';
+import { PaisesService } from './servicios/paises.service';
+import { JugadoresService } from './servicios/jugadores.service';
+import { JuegoServiceService } from './servicios/juego-service.service';
+
+import { SexoPipe } from './pipes/sexo.pipe';
+
 // declaro donde quiero que se dirija
 /*
 const MiRuteo = [{path: 'error' , component: ErrorComponent},
@@ -38,21 +59,7 @@ const MiRuteo = [{path: 'error' , component: ErrorComponent},
 
 {path: '**' , component: ErrorComponent} ];
 */
-import { JugadoresListadoComponent } from './componentes/jugadores-listado/jugadores-listado.component';
 
-import { JuegoServiceService } from './servicios/juego-service.service';
-import { ListadosComponent } from './componentes/listados/listados.component';
-import { JuegosComponent } from './componentes/juegos/juegos.component';
-import { RegistroComponent } from './componentes/registro/registro.component';
-import { MenuCardComponent } from './componentes/menu-card/menu-card.component';
-import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
-import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
-import { AnagramaComponent } from './componentes/anagrama/anagrama.component';
-import { ListadoDePaisesComponent } from './componentes/listado-de-paises/listado-de-paises.component';
-import { MapaDeGoogleComponent } from './componentes/mapa-de-google/mapa-de-google.component'
-import { AgmCoreModule } from '@agm/core';
-import { InputJugadoresComponent } from './componentes/input-jugadores/input-jugadores.component';
-import { SexoPipe } from './pipes/sexo.pipe';
 
 @NgModule({
   declarations: [
@@ -83,6 +90,9 @@ import { SexoPipe } from './pipes/sexo.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
     RuteandoModule,
     HttpModule,
     AgmCoreModule.forRoot({
@@ -92,7 +102,12 @@ import { SexoPipe } from './pipes/sexo.pipe';
     // importo el ruteo
     // RouterModule.forRoot(MiRuteo)
   ],
-  providers: [ JuegoServiceService, MiHttpService,PaisesService,ArchivosJugadoresService,JugadoresService],
-  bootstrap: [AppComponent]
+  exports: [
+    MatIconModule
+  ],
+  providers: [ JuegoServiceService, MiHttpService, PaisesService, ArchivosJugadoresService, JugadoresService],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+
 })
 export class AppModule { }
