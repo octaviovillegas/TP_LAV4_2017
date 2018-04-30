16232a3bf55cb552f77b510e72ad301a98d70f51
German Schmidt <user.name = German Schmidt>
2018-04-29 23:50:59 -0300

    Ultimos cambios pantalla principal cards de links

--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PaisesService } from 'app/servicios/paises.service'
 
 @Component({
   selector: 'app-principal',
@@ -7,15 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
- public status: any = {
+
+  public status: any = {
     isFirstOpen: true,
     isFirstDisabled: false
   };
-  constructor() {  }
+
+  public listadoPaises = new Array<any>();
+  public paisSeleccionado = {};
+  constructor(private servicioPaises: PaisesService) {  }
 
   ngOnInit() {
   }
 
- 
+  verPaises(){
+    this.servicioPaises.listar().then((response)=>{
+      this.listadoPaises = response;
+    }).catch((error)=>{
+      console.log("error al listar los paises")
+    })
+  }
+
+  paisSeleccionadoClick(pais){
+    this.paisSeleccionado = pais;
+  }
+
+
 
 }
